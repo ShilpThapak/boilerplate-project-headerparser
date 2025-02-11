a78856a5f2e83f3339d3329ef9379bad2311509f
Shilp Thapak <thapak.shilp2395@gmail.com>
2025-02-11 06:52:01 +0000

    Added request params

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function (req, res) {
+  response = {
+    "ipaddress": req.rawHeaders[req.rawHeaders.length - 7],
+    "language": req.rawHeaders[9],
+    "software": req.rawHeaders[3],
+  }
+  res.json(response);
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
